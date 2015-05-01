54877dbdb252e07a4fd54203e8e18b73440baf0e
Stuart Memo <stuartmemo@gmail.com>
2015-05-01 08:26:33 +0100

    Added synth code.

--- scripts/synth.js
+++ scripts/synth.js
@@ -4,3 +4,40 @@ var keyboard = new QwertyHancock({
      height: 150,
      octaves: 2
 });
+
+var context = new AudioContext(),
+    masterVolume = context.createGain(),
+    oscillators = {};
+
+masterVolume.gain.value = 0.2;
+
+masterVolume.connect(context.destination);
+
+keyboard.keyDown = function (note, frequency) {
+    var osc = context.createOscillator(),
+        osc2 = context.createOscillator();
+
+    osc.frequency.value = frequency;
+    osc.type = 'sawtooth';
+    osc.detune.value = -10;
+
+    osc2.frequency.value = frequency;
+    osc2.type = 'triangle';
+    osc2.detune.value = 10;
+
+    osc.connect(masterVolume);
+    osc2.connect(masterVolume);
+
+    masterVolume.connect(context.destination);
+
+    oscillators[frequency] = [osc, osc2];
+
+    osc.start(context.currentTime);
+    osc2.start(context.currentTime);
+};
+
+keyboard.keyUp = function (note, frequency) {
+    oscillators[frequency].forEach(function (oscillator) {
+        oscillator.stop(context.currentTime);
+    });
+};
